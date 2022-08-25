febfc5e5f9b0ef043d067f61911e656471d5d8a6
sfyyy <751289526@qq.com>
2022-08-25 23:44:25 +0800

    feat(init): init

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import utools from "vite-plugin-utools";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+  server: {
+    port: 3000,
+  },
+  publicDir: "plugin",
+  plugins: [
+    vue(),
+    utools({
+      external: "uTools",
+      preload: {
+        path: "./plugin/preload.ts",
+        watch: true,
+        name: "window.preload",
+      },
+      buildUpx: {
+        pluginPath: "./plugin/plugin.json",
+        outDir: "upx",
+        outName: "[pluginName]_[version].upx",
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
